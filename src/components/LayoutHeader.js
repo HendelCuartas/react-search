39e59791a669f55506d678bc4efd290535effc74
Hendel Cuartas <hendel@foostudio.mx>
2018-11-29 11:40:49 -0600

    feat(components/LayoutHeader): component created

---
+++ src/components/LayoutHeader.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/components/LayoutHeader.styl'
+
+class LayoutHeader extends React.Component {
+    render() {
+        return (
+            <div className="LayoutHeader">
+
+            </div>
+        );
+    }
+}
+
+export default LayoutHeader;
