1b2c45dae57e4b03c00141c622900c4cdb6a8581
Hendel Cuartas <hendel@foostudio.mx>
2018-11-29 12:03:43 -0600

    feat(webpack.config.js)

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
     },
     output: {
         path: __dirname + '/docs',
-        publicPath: '/react-search/',
+        publicPath: '/',
         filename: 'bundle.js'
     },
     plugins: [
