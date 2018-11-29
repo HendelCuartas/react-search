b96082546d37ae21accd3522c991e0e835da92e0
Hendel Cuartas <hendel@foostudio.mx>
2018-11-29 11:41:30 -0600

    feat(index.js): refactoring App to a component

--- src/index.js
+++ src/index.js
@@ -1,27 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.styl';
-import { TweenMax, Power2 } from "gsap/all";
-
-class App extends React.Component {
-    constructor(props){
-        super(props);
-        // logo container
-        this.title = null;
-    }
-
-    componentDidMount(){
-        TweenMax.fromTo(this.title, 0.5, {opacity: 0, y: -40}, {opacity: 1, y: 0, ease: Power2.easeIn})
-    }
-
-    render() {
-        return (
-            <div className="Home">
-                <h1 ref={ h1 => this.title = h1 }>Hello there!</h1>
-            </div>
-        );
-    }
-}
+import './styles/reset-css.styl';
+import App from './components/App';
 
 ReactDOM.render(
     <App />,
