eac3aedddbfafe7ad8c2dc2ba80bd657d97e5100
Hendel Cuartas <hendel@foostudio.mx>
2018-11-29 12:35:43 -0600

    feat(components/LayoutHeader): import and use of search bar component

--- src/components/LayoutHeader.js
+++ src/components/LayoutHeader.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../styles/components/LayoutHeader.styl'
+import SearchBar from '../components/SearchBar'
 
 class LayoutHeader extends React.Component {
     render() {
         return (
             <div className="LayoutHeader">
-
+                <SearchBar></SearchBar>
             </div>
         );
     }
