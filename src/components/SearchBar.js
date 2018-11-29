b45aeafc1f5a9a140d2633dfae15e5e8224fba06
Hendel Cuartas <hendel@foostudio.mx>
2018-11-29 14:51:23 -0600

    feat(components/SearchVar): add styles and onChanche event to input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import '../styles/components/SearchBar.styl'
 
 class SearchBar extends React.Component {
+
+    onInputChange(event) {
+        console.log(event.target.value)
+    }
+
     render() {
         return (
-            <div>
-                <form>
-                    <input type="text"/>
+            <div className="SearchBar">
+                <form className="SearchBar-form">
+                    <input className="SearchBar-input" type="text" onChange={this.onInputChange}>
+                    </input>
+                    <svg className="SearchBar-icon">
+                        <use href="#icon-search"></use>
+                    </svg>
                 </form>
             </div>
         );
