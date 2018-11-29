0c2509c933b4d638eab99c20690abc5211ad57c2
Hendel Cuartas <hendel@foostudio.mx>
2018-11-29 12:35:12 -0600

    feat(componetns/Background): create component

---
+++ src/components/Background.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/components/Background.styl'
+
+const Background = () => {
+    return (
+        <div className="Background"></div>
+    );
+}
+
+export default Background;
