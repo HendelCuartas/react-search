c40fa6ecded3284931a6e6d339b3f60e9c092048
Hendel Cuartas <hendel@foostudio.mx>
2018-12-04 17:49:02 -0600

    feat(components/ImageList): import and use of ImageCard component

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import '../styles/components/ImageList.styl'
+import '../styles/components/ImageList.styl';
+import ImageCard from './ImageCard';
 
 const ImageList = props => {
     const imagesList = props.images.map((image) => {
         return (
-            <img
-                className="ImageList-item"
-                src={image.urls.regular}
-                key={image.id}
-                alt={image.description}
-            ></img>
+            <ImageCard className="ImageList-item" key={image.id} image={image}></ImageCard>
         );
     });
     return <div className="ImageList">{imagesList}</div>
