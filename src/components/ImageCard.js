4f8905765a23ee241903b9183de6eb89a9cc255a
Hendel Cuartas <hendel@foostudio.mx>
2018-12-04 17:48:38 -0600

    feat(components/ImageCard): component created and templated

---
+++ src/components/ImageCard.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import '../styles/components/ImageCard.styl'
+
+class ImageCard extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { spans: 0 };
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.imageRef.current.addEventListener('load', this.setSpans);
+    }
+
+    setSpans = () => {
+        const height = this.imageRef.current.clientHeight;
+        const spans = Math.ceil(height / 10);
+        this.setState({ spans });
+    }
+
+    render() {
+        return (
+            <div className="ImageCard" style={{ gridRowEnd: `span ${this.state.spans}` }}>
+                <img
+                    src={this.props.image.urls.regular}
+                    alt={this.props.image.description}
+                    className="ImageCard-image"
+                    ref={this.imageRef}
+                />
+            </div>
+        );
+    }
+};
+
+export default ImageCard;
