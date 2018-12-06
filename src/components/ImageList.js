362b6a7fca86c9ec678a4da5cf100053aefddb61
Hendel Cuartas <hendel@foostudio.mx>
2018-12-06 16:02:24 -0600

    feat(components/ImageList): conver component from function to class, set template when no results are found and add animations

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,14 +1,55 @@
 import React from 'react';
+import TweenMax from 'gsap';
 import '../styles/components/ImageList.styl';
 import ImageCard from './ImageCard';
 
-const ImageList = props => {
-    const imagesList = props.images.map((image) => {
+class ImageList extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.listRef = React.createRef();
+    }
+
+    onClickImage(e) {
+        this.props.onClickImageApp(e);
+    }
+
+    componentDidMount() {
+        TweenMax.fromTo(this.listRef.current, 0.5, {opacity: 0, y: -40}, {opacity: 1, y: 0, ease: Power2.easeIn})
+    }
+
+    render() {
+        if (this.props.images.length > 0) {
+            this.imagesList = this.props.images.map((image) => {
+                return (
+                    <ImageCard
+                        className="ImageList-item"
+                        key={image.id}
+                        image={image}
+                        onClickImage={e => this.onClickImage(e)}
+                    ></ImageCard>
+                );
+            });
+        } else {
+            const NoResults = () => {
+                return (
+                    <div style={{
+                        textAlign: 'center',
+                        position: 'absolute',
+                        width: '100%',
+                    }}>
+                        <h1>No se encontraron resultados, intenta con otra palabra.</h1>
+                    </div>
+                );
+            }
+            this.imagesList = (<NoResults></NoResults>);
+        }
         return (
-            <ImageCard className="ImageList-item" key={image.id} image={image}></ImageCard>
+            <div className="ImageList" ref={this.listRef}>
+                {this.imagesList}
+            </div>
         );
-    });
-    return <div className="ImageList">{imagesList}</div>
-};
+    }
+}
 
 export default ImageList;
